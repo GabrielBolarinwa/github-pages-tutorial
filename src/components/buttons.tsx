42c4dcc1cd04976a1b37fed8070a2ac08f94b4b1
Gabriel <gabibola955@gmail.com>
2026-01-23 16:08:20 +0100

    Update Buttons Text

--- src/components/buttons.tsx
+++ src/components/buttons.tsx
@@ -7,13 +7,13 @@ function Buttons(props: props) {
   return (
     <div className="buttonGroup">
       <button className="countButtons increment" onClick={props.increment}>
-        Increment
+        Increment By 1
       </button>
       <button className="countButtons reset" onClick={props.reset}>
         Reset
       </button>
       <button className="countButtons decrement" onClick={props.decrement}>
-        Decrement
+        Decrement By 1
       </button>
     </div>
   );
